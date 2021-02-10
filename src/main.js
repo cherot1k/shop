6e17eab7eabab26b745a393e2dd183bb376dba80
kunter555 <lagent073@gmail.com>
2021-02-10 10:45:54 +0200

    commit

--- src/main.js
+++ src/main.js
@@ -5,20 +5,9 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import '@/assets/fonts.css'
-import firebase from "firebase/app";
+import init from "@/firebase/firebase_init"
 Vue.config.productionTip = false
-
-var firebaseConfig = {
-  apiKey: "AIzaSyDL5p1aGkZWDe9qlvY0Kn-n2RMtg__xTmk",
-  authDomain: "framessite-244e2.firebaseapp.com",
-  projectId: "framessite-244e2",
-  storageBucket: "framessite-244e2.appspot.com",
-  messagingSenderId: "43789150410",
-  appId: "1:43789150410:web:219e69e8330cc4f8952c33"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
+init()
 
 new Vue({
   router,
